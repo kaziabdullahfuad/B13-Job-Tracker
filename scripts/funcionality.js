98a4805979918a71d82acee6639e9aea61de4e9d
Kazi Abdullah Fuad <kaziabdullahfuad@gmail.com>
2026-03-14 14:50:57 +0600

    Worked in delete button

--- scripts/funcionality.js
+++ scripts/funcionality.js
@@ -1,5 +1,6 @@
 let interviewList=[];
 let rejectedList=[];
+let currentStatus='all';
 
 // all the total,interview and rejected count cards
 let total_count=document.getElementById('total-tracker-count');
@@ -39,29 +40,62 @@ function toggleStyle(id){
     allRejectedbtntab.classList.add('text-[#64748B]', 'bg-[#FFFFFF]','border-[#F1F2F4]', 'border-1','shadow-md');
 
     const selected=document.getElementById(id);
-    console.log(selected);
+    // console.log(selected);
+    currentStatus=id; // now current status id te to find kon tab a achi
 
     selected.classList.remove('text-[#64748B]', 'bg-[#FFFFFF]','shadow-md','border-[#F1F2F4]', 'border-1');
     selected.classList.add('bg-[#3B82F6]', 'text-[#FFFFFF]');
 
     if(id=='interview-btn-tab'){
         document.getElementById('job-container').classList.add('hidden');
+        console.log("Interview  list len",interviewList);
         if(interviewList.length==0){
             document.getElementById('available-job-count').innerText=interviewList.length;
             document.getElementById('no-jobs-available').classList.remove('hidden');
+             document.getElementById('filtered-section').classList.add('hidden');
         }
         else{
             console.log("Array size",interviewList.length);
             document.getElementById('available-job-count').innerText=interviewList.length;
             document.getElementById('filtered-section').classList.remove('hidden');
+            document.getElementById('no-jobs-available').classList.add('hidden');
             renderInterview();
         }
     }
     else if(id=='all-btn-tab'){
         document.getElementById('available-job-count').innerText=document.getElementById('job-container').children.length;
-        document.getElementById('no-jobs-available').classList.add('hidden');
-        document.getElementById('job-container').classList.remove('hidden');
-        document.getElementById('filtered-section').classList.add('hidden');
+        if(document.getElementById('job-container').children.length==0){
+            console.log("Amar kichui nai");
+            
+            document.getElementById('filtered-section').classList.add('hidden');
+            document.getElementById('job-container').classList.add('hidden');
+            document.getElementById('no-jobs-available').classList.remove('hidden');
+        }
+        else{
+            document.getElementById('no-jobs-available').classList.add('hidden');
+            document.getElementById('job-container').classList.remove('hidden');
+            document.getElementById('filtered-section').classList.add('hidden');
+        }
+        
+        
+        
+    }
+    else if(id=='rejected-btn-tab'){
+        document.getElementById('job-container').classList.add('hidden');
+        if(rejectedList.length==0){
+           
+            document.getElementById('available-job-count').innerText=rejectedList.length;
+            document.getElementById('no-jobs-available').classList.remove('hidden');
+            document.getElementById('filtered-section').classList.add('hidden');
+        }
+        else{
+            console.log("Array size in rejected btn tab",rejectedList.length);
+            document.getElementById('available-job-count').innerText=rejectedList.length;
+            document.getElementById('filtered-section').classList.remove('hidden');
+            document.getElementById('no-jobs-available').classList.add('hidden');
+            renderRejected();
+           
+        }
     }
     
 }
@@ -100,11 +134,75 @@ job_contain.addEventListener('click',function(event){
             parentNode.querySelector('.job-badge').innerText='INTERVIEW'
             interviewList.push(jobInfo);
         }
-        total_card();
-        // renderInterview();
-        // console.log(interviewList.length);        
-        // console.log(interviewList);
         
+        const foundInRejected=rejectedList.find(item=>item.companyName==companyName);
+        if(foundInRejected){
+            // will have to change the style of the badge
+            parentNode.querySelector('.job-badge').classList.remove('badge-error');
+            rejectedList=rejectedList.filter(value=>value.companyName!=companyName);
+            
+            // kon tab a achi r
+           
+        }
+        
+        total_card();
+      
+        
+    }
+    else if(event.target.classList.contains('rejected-btn')){
+        const parentNode=event.target.parentNode.parentNode;
+        const companyName=parentNode.querySelector('.company-name').innerText;
+        const jobPosition=parentNode.querySelector('.job-position').innerText;
+        const jobSalary=parentNode.querySelector('.job-salary').innerText;
+        const jobBadge=parentNode.querySelector('.job-badge').innerText;
+        const jobDescription=parentNode.querySelector('.job-description').innerText;
+
+        console.log(companyName,jobPosition,jobSalary,jobBadge,jobDescription);
+
+        // now ekta object banabo
+        const jobInfo={
+            companyName,
+            jobPosition,
+            jobSalary,
+            jobBadge,
+            jobDescription
+        }
+        
+        // console.log(jobInfo);
+
+        const jobExists=rejectedList.find(item=> item.companyName==jobInfo.companyName)
+
+        if(!jobExists){
+            parentNode.querySelector('.job-badge').classList.remove('badge-soft','badge-primary');
+            parentNode.querySelector('.job-badge').classList.add('badge-error');
+            parentNode.querySelector('.job-badge').innerText='REJECTED'
+            rejectedList.push(jobInfo);
+        }
+
+        const foundInRejected=interviewList.find(item=>item.companyName==companyName);
+        if(foundInRejected){
+            // will have to change the style of the badge
+            parentNode.querySelector('.job-badge').classList.remove('badge-success');
+            interviewList=interviewList.filter(value=>value.companyName!=companyName);
+        }
+
+        if(currentStatus=='interview-btn-tab'){
+            // interview tab a ache
+            console.log("Interview  list len",interviewList);
+            if(interviewList.length==0){
+                // console.log("Interview  list len",interviewList);
+                
+                 document.getElementById('no-jobs-available').classList.remove('hidden');
+                  document.getElementById('filtered-section').classList.add('hidden');
+                // renderInterview();
+            }
+            else{
+                document.getElementById('no-jobs-available').classList.add('hidden');
+                renderInterview();
+            }
+        }
+
+        total_card();
     } 
     
     
@@ -141,6 +239,48 @@ function renderInterview(){
                 <p class="job-salary my-5 text-[#64748B] text-sm">${interview.jobSalary}</p>
                 <div class="job-badge badge badge-success text-[#002C5C] p-4">INTERVIEW</div>
                 <p class="job-description text-[#323B49] text-sm mt-2 mb-5">${interview.jobDescription}</p>
+
+                <!-- duita button banao -->
+                 <div class="flex gap-2">
+                    <button class="interview-btn text-[#10B981] py-2 px-3 font-semibold border-1 cursor-pointer hover:bg-green-300">INTERVIEW</button>
+                    <button class="rejected-btn text-[#EF4444] py-2 px-3 font-semibold border-1 cursor-pointer hover:bg-red-300">REJECTED</button>
+                 </div>
+
+        </div>
+        `
+        filterSection.appendChild(div);
+        
+    }
+}
+
+// for rejected rendering
+function renderRejected(){
+    
+    filterSection.innerHTML='';
+    let div=document.createElement('div');
+    div.className='jobs-information-container grid grid-cols-1 gap-4';
+    for(let rejected of rejectedList){
+        
+        console.log(rejected);
+        
+        // let div=document.createElement('div');
+        // div.className='jobs-information-container grid grid-cols-1 gap-4';
+        div.innerHTML+=`
+            <div class="jobs-info-card p-6 bg-[#FFFFFF] shadow-md border-[#F1F2F4] rounded-md border-2">
+                <div class="flexing-card-button flex justify-between items-center">
+                    <div>
+                        <h2 class="company-name text-[#002C5C] font-semibold mb-1 text-xl">${rejected.companyName}</h2>
+                        <p class="job-position text-[#64748B]">${rejected.jobPosition}</p>
+
+                    </div>
+                    <div class="w-8 rounded-full bg-[#FFFFFF]">
+
+                        <button class="cursor-pointer "><img src="./images/Trash.png" alt=""></button>
+                    </div>
+                </div>
+                <p class="job-salary my-5 text-[#64748B] text-sm">${rejected.jobSalary}</p>
+                <div class="job-badge badge badge-error text-[#002C5C] p-4">REJECTED</div>
+                <p class="job-description text-[#323B49] text-sm mt-2 mb-5">${rejected.jobDescription}</p>
 
                 <!-- duita button banao -->
                  <div class="flex gap-2">
